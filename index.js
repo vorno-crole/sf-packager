b5eae272ba2af457b6bc14882b763718128029f9
Vaughan Crole <vaughan.crole@au1.ibm.com>
2020-02-12 16:39:50 +1100

    Initial commit

--- index.js
+++ index.js
@@ -12,149 +12,225 @@
  *  and copy each metadata item into a matching folder.
  *  Also if any deletes occurred it will create a file at ./deploy/featureBranch/destructive/destructiveChanges.xml
  */
-const program = require('commander');
-const spawnSync = require('child_process').spawnSync;
-const packageWriter = require('./lib/metaUtils').packageWriter;
-const buildPackageDir = require('./lib/metaUtils').buildPackageDir;
-const copyFiles = require('./lib/metaUtils').copyFiles;
-const packageVersion = require('./package.json').version;
+
+var program = require('commander');
+var util = require('util'),
+	spawnSync = require('child_process').spawnSync,
+	packageWriter = require('./lib/metaUtils').packageWriter,
+	buildPackageDir = require('./lib/metaUtils').buildPackageDir,
+	copyFiles = require('./lib/metaUtils').copyFiles,
+	packageVersion = require('./package.json').version;
+var debugMsgs = false;
+
 
 program
-    .arguments('<compare> <branch> [target]')
-    .version(packageVersion)
-    .option('-d, --dryrun', 'Only print the package.xml and destructiveChanges.xml that would be generated')
-    .option('-p, --pversion [version]', 'Salesforce version of the package.xml', parseInt)
-    .action(function (compare, branch, target) {
+	.arguments('<compare> <branch> [target]')
+	.version(packageVersion)
+	.option('-d, --dryrun', 'Only print the package.xml and destructiveChanges.xml that would be generated')
+	.option('-p, --pversion [version]', 'Salesforce version of the package.xml', parseInt)
+	.option('-x, --destructive', 'Only include destructive (deleted) changes')
+	.option('-s, --src <source directory>', 'Root source directory. Defaults to force-app', 'force-app')
+	.action(function (compare, branch, target) {
 
-        if (!branch || !compare) {
-            console.error('branch and target branch are both required');
-            program.help();
-            process.exit(1);
-        }
+		if (!branch || !compare) {
+			console.error('branch and target branch are both required');
+			program.help();
+			process.exit(1);
+		}
 
-        const dryrun = program.dryrun;
+		console.log('Running sfpackage');
 
-        if (!dryrun && !target) {
-            console.error('target required when not dry-run');
-            program.help();
-            process.exit(1);
-        }
+		var dryrun = false;
+		if (program.dryrun) {
+			dryrun = true;
+		}
 
-        const currentDir = process.cwd();
-        const gitDiff = spawnSync('git', ['--no-pager', 'diff', '--name-status', compare, branch]);
-        const gitDiffStdOut = gitDiff.stdout.toString();
-        const gitDiffStdErr = gitDiff.stderr.toString();
+		var destructiveOnly = false;
+		if (program.destructive) {
+			console.log('* Only including destructive changes.');
+			destructiveOnly = true;
+		}
 
-        if (gitDiffStdErr) {
-            console.error('An error has occurred: %s', gitDiffStdErr);
-            process.exit(1);
-        }
+		if (!dryrun && !target) {
+			console.error('target required when not dry-run');
+			program.help();
+			process.exit(1);
+		}
 
-        let fileListForCopy = [];
+		var currentDir = process.cwd();
 
-        //defines the different member types
-        const metaBag = {};
-        const metaBagDestructive = {};
-        let deletesHaveOccurred = false;
+		// compare is "existing"
+		// branch is new changes I wish to merge
+		const gitDiff = spawnSync('git', ['diff', '--name-status', '--no-renames', compare, branch]);
+		
+		var gitDiffStdOut = gitDiff.stdout.toString('utf8');
+		var gitDiffStdErr = gitDiff.stderr.toString('utf8');
 
-        const fileList = gitDiffStdOut.split('\n');
-        fileList.forEach(function (fileName) {
+		if (gitDiffStdErr) {
+			console.error('An error has occurred: %s', gitDiffStdErr);
+			process.exit(1);
+		}
 
-            // get the git operation
-            const operation = fileName.slice(0,1);
-            // remove the operation and spaces from fileName
-            fileName = fileName.slice(1).trim();
+		var fileListForCopy = [],
+			fileList = [];
 
-            //ensure file is inside of src directory of project
-            if (fileName && fileName.substring(0,3) === 'src') {
+		//defines the different member types
+		var metaBag = {};
+		var metaBagDestructive = {};
+		var deletesHaveOccurred = false;
+		var rootdir = program.src;
+		if (debugMsgs) console.log('rootdir: ' + rootdir);
 
-                //ignore changes to the package.xml file
-                if(fileName === 'src/package.xml') {
-                    return;
-                }
+		fileList = gitDiffStdOut.split('\n');
+		fileList.forEach(function (fileName, index)
+		{
+			// get the git operation
+			var operation = fileName.slice(0,1);
+			// remove the operation and spaces from fileName
+			fileName = fileName.slice(1).trim();
+			if (debugMsgs) console.log('fileName: ' + fileName);
 
-                const parts = fileName.split('/');
-                // Check for invalid fileName, likely due to data stream exceeding buffer size resulting in incomplete string
-                // TODO: need a way to ensure that full fileNames are processed - increase buffer size??
-                if (parts[2] === undefined) {
-                    console.error('File name "%s" cannot be processed, exiting', fileName);
-                    process.exit(1);
-                }
+			//ensure file is inside of src directory of project
+			if (fileName && fileName.substring(0, rootdir.length) === rootdir)
+			{
+				//ignore changes to the package.xml file
+				if (fileName === rootdir+'/package.xml')
+				{
+					return;
+				}
 
-                let meta;
+				var parts = fileName.split('/');
 
-                if (parts.length === 4) {
-                    // Processing metadata with nested folders e.g. emails, documents, reports
-                    meta = parts[2] + '/' + parts[3].split('.')[0];
-                } else {
-                    // Processing metadata without nested folders. Strip -meta from the end.
-                    meta = parts[2].split('.')[0].replace('-meta', '');
-                }
+				// SFDX has some strange folder hierarchies compared to
+				// old-school metadata, so we need to tidy these up for this
+				// legacy program.
+				var removeParts = ["main", "default"];
 
-                if (operation === 'A' || operation === 'M') {
-                    // file was added or modified - add fileName to array for unpackaged and to be copied
-                    console.log('File was added or modified: %s', fileName);
-                    fileListForCopy.push(fileName);
+				var partsLength = parts.length;
+				for (var i = 0; i < partsLength; i++)
+				{
+					if (removeParts.includes(parts[i]))
+					{
+						if (debugMsgs) console.log('Removing: ' + parts[i]);
+						parts.splice(i, 1);
+						i--;
+					}
+				}
 
-                    if (!metaBag.hasOwnProperty(parts[1])) {
-                        metaBag[parts[1]] = [];
-                    }
+				if (debugMsgs) console.log('parts: ' + parts);
+				if (debugMsgs) console.log('parts.length: ' + parts.length);
 
-                    if (metaBag[parts[1]].indexOf(meta) === -1) {
-                        metaBag[parts[1]].push(meta);
-                    }
-                } else if (operation === 'D') {
-                    // file was deleted
-                    console.log('File was deleted: %s', fileName);
-                    deletesHaveOccurred = true;
+				// Check for invalid fileName, likely due to data stream exceeding buffer size resulting in incomplete string
+				// TODO: need a way to ensure that full fileNames are processed - increase buffer size??
+				if (parts[2] === undefined) {
+					console.error('File name "%s" cannot be processed, exiting', fileName);
+					process.exit(1);
+				}
 
-                    if (!metaBagDestructive.hasOwnProperty(parts[1])) {
-                        metaBagDestructive[parts[1]] = [];
-                    }
+				var meta;
+				var metaProperty = parts[1];
 
-                    if (metaBagDestructive[parts[1]].indexOf(meta) === -1) {
-                        metaBagDestructive[parts[1]].push(meta);
-                    }
-                } else {
-                    // situation that requires review
-                    return console.error('Operation on file needs review: %s', fileName);
-                }
-            }
-        });
+				if (parts.length === 4)
+				{
+					// Processing metadata with nested folders e.g. emails, documents, reports
+					meta = parts[2] + '/' + parts[3].split('.')[0];
+				}
+				else if (parts.length === 5 && (parts[3] == 'fields' || parts[3] == 'validationRules'))
+				{
+					// Processing metadata with nested folders e.g. emails, documents, reports
+					meta = parts[2] + '.' + parts[4].split('.')[0];
+					metaProperty = parts[3];
+				}
+				else
+				{
+					// Processing metadata without nested folders. Strip -meta from the end.
+					meta = parts[2].split('.')[0].replace('-meta', '');
+				}
 
-        // build package file content
-        const packageXML = packageWriter(metaBag, program.pversion);
-        // build destructiveChanges file content
-        const destructiveXML = packageWriter(metaBagDestructive, program.pversion);
-        if (dryrun) {
-            console.log('\npackage.xml\n');
-            console.log(packageXML);
-            console.log('\ndestructiveChanges.xml\n');
-            console.log(destructiveXML);
-            process.exit(0);
-        }
+				if (debugMsgs) console.log('meta: ' + meta);
 
-        console.log('Building in directory %s', target);
+				if (operation === 'A' || operation === 'M')
+				{
+					if (!destructiveOnly)
+					{
+						// file was added or modified - add fileName to array for unpackaged and to be copied
+						console.log('File was added or modified: %s', fileName);
+						
+						
+						// TODO: add flag here to not copy.
+						fileListForCopy.push(fileName);
 
-        buildPackageDir(target, branch, metaBag, packageXML, false, (err, buildDir) => {
-            if (err) {
-                return console.error(err);
-            }
+						if (!metaBag.hasOwnProperty(metaProperty)) {
+							metaBag[metaProperty] = [];
+						}
 
-            copyFiles(currentDir, buildDir, fileListForCopy);
-            console.log('Successfully created package.xml and files in %s',buildDir);
-        });
+						if (metaBag[metaProperty].indexOf(meta) === -1) {
+							metaBag[metaProperty].push(meta);
+						}
+					}
+				}
+				else if (operation === 'D')
+				{
+					// file was deleted
+					console.log('File was deleted: %s', fileName);
+					deletesHaveOccurred = true;
 
-        if (deletesHaveOccurred) {
-            buildPackageDir(target, branch, metaBagDestructive, destructiveXML, true, (err, buildDir) => {
+					if (!metaBagDestructive.hasOwnProperty(metaProperty)) {
+						metaBagDestructive[metaProperty] = [];
+					}
 
-                if (err) {
-                    return console.error(err);
-                }
+					if (metaBagDestructive[metaProperty].indexOf(meta) === -1) {
+						metaBagDestructive[metaProperty].push(meta);
+					}
+				}
+				else
+				{
+					// situation that requires review
+					return console.error('Operation on file needs review: %s', fileName);
+				}
+			}
+		});
 
-                console.log('Successfully created destructiveChanges.xml in %s',buildDir);
-            });
-        }
-    });
+		//build package file content
+		var packageXML = packageWriter(metaBag, program.pversion);
+
+		//build destructiveChanges file content
+		var destructiveXML = packageWriter(metaBagDestructive, program.pversion);
+		if (dryrun) {
+			if (!destructiveOnly)
+			{
+				console.log('\npackage.xml\n');
+				console.log(packageXML);
+			}
+			console.log('\ndestructiveChanges.xml\n');
+			console.log(destructiveXML);
+			process.exit(0);
+		}
+
+		console.log('Building in directory %s', target);
+
+		// TODO: add flag here to not copy.
+		buildPackageDir(target, branch, metaBag, packageXML, false, destructiveOnly, (err, buildDir) => {
+
+			if (err) {
+				return console.error(err);
+			}
+
+			copyFiles(currentDir, buildDir, fileListForCopy);
+			console.log('Successfully created package.xml and files in %s', buildDir);
+
+		});
+
+		if (deletesHaveOccurred) {
+			buildPackageDir(target, branch, metaBagDestructive, destructiveXML, true, destructiveOnly, (err, buildDir) => {
+
+				if (err) {
+					return console.error(err);
+				}
+
+				console.log('Successfully created destructiveChanges.xml in %s',buildDir);
+			});
+		}
+	});
 
 program.parse(process.argv);
--- readme.md
+++ readme.md
@@ -1,11 +1,37 @@
 # Overview
 
-CLI Tool to generate Salesforce.com package.xml (and destructiveChange.xml) files based on git diff between two branches. 
+CLI Tool to generate Salesforce.com package.xml (and destructiveChange.xml) files based on git diff between two commits or branches. 
+
+## Prerequisite
+
+This tool requires Node JS. Install it from here:  
+https://nodejs.org/en/download/  
 
 ## Install
 
+If you can see this page, you can install it.  
+You do not require repo write access to install this tool.  
+
+To install, using the green `Clone or download` button above, download the Zip of this repo.  
+Unzip it somewhere convenient and open a terminal window in that directory. Then run  
+
 ```
-npm install -g sf-packager
+npm link
+```
+
+Depending on your computer's configuration, you may need to use `sudo` to do this.
+
+![Terminal screenshot](images/install.png)
+
+## Install for development
+
+If you need to contribute, modify or amend this package, clone the repo (if you don't know how, try Github Desktop), and install from the cloned directory.
+
+Command line representation here:
+```
+git clone https://github.ibm.com/BLUEWOLFANZ/sf-packager
+cd sf-packager
+npm link
 ```
 
 ## Usage
@@ -24,11 +50,24 @@ You can force a specific version for the package.xml with the -p flag
 sfpackage destinationBranch sourceBranch -p 42 ./deploy/
 ```
 
-You can also just write the package.xml and destructiveChanges.xml by passing the -d flag
+You can also just print the package.xml and destructiveChanges.xml by passing the -d flag
 
 ```
 sfpackage destinationBranch sourceBranch -d > ~/Desktop/packageAndDestructiveChanges.xml
 ```
+
+You can only include the destructive changes with the -x flag
+
+```
+sfpackage -x destinationBranch sourceBranch destructive/
+```
+
+The -s (--src) flag allows you to nominate your source root directory. This defaults to force-app.
+
+```
+sfpackage destinationBranch sourceBranch destructive/ -s src
+```
+
 
 You can also create "backout" content by reversing the order of the destination and source branches
 
@@ -37,3 +76,6 @@ sfpackage sourceBranch destinationBranch ./deploy/
 ```
 
 
+## Original code
+
+This repo is a fork from https://github.com/dlively1/sf-packager
